67a8a9e9fa96d1c59d9dcd6f36fa6dcd93c8e233
Alan <macbookpro@MacBooks-MacBook-Pro.local>
2016-07-04 16:52:37 -0500

    add high low game

---
+++ high_low.php
@@ -0,0 +1,16 @@
+<?php
+
+$number = mt_rand(1, 100);
+fwrite(STDOUT, "What number am I thinking?\n");
+$guess = fgets(STDIN);
+do {
+	if ($guess > $number) {
+		fwrite(STDOUT, "Lower!\n");
+		$guess = fgets(STDIN);
+	} elseif ($guess < $number) {
+		fwrite(STDOUT, "Higher!\n");
+		$guess = fgets(STDIN);
+	} elseif ($guess == $number) {
+		fwrite(STDOUT, "You win!\n");
+	}
+} while ($guess !== $number);
