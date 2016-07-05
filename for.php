13030cd5406599279772d06eac9181cd9615f613
Alan <macbookpro@MacBooks-MacBook-Pro.local>
2016-07-05 12:20:45 -0500

    "Finished for loop exercise"

--- for.php
+++ for.php
@@ -3,6 +3,16 @@ echo "Give me a starting number\n";
 $i = trim(fgets(STDIN));
 echo "Give me an ending number\n";
 $a = trim(fgets(STDIN));
-for ($i = $i; $i <= $a; $i++) {
-	echo "$i\n";
+echo "Would you like to set a custom increment?\n";
+$b = trim(fgets(STDIN));
+for ($i = $i; $i <= $a; $i+=$b) {
+	if ($b == null) {
+		$i++;
+	}
+	if (is_numeric($i) && is_numeric($a)) {
+		echo "$i\n";
+	} else {
+		echo "make sure you pass numeric values\n";
+		exit(0);
+	}
 }
