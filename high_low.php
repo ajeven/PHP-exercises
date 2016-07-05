7440a72dd134539c13c2d9408044a6c3dc11508f
Alan <macbookpro@MacBooks-MacBook-Pro.local>
2016-07-05 10:35:12 -0500

    Fixed bugs with arguments

--- high_low.php
+++ high_low.php
@@ -1,25 +1,27 @@
 <?php
 
-$number = mt_rand($argv[1], $argv[2]);
-fwrite(STDOUT, "What number am I thinking?\n");
 // Best practice to always trim STDIN
-$guess = trim(fgets(STDIN));
-$count = 0;
-if ($argc == 3) {
+$count = 1;
+if ($argc == 3 && is_numeric($argv[1])  && is_numeric($argv[2]) && $argv[1] <= $argv[2]) {
 	echo "The number you are looking for is between $argv[1] and $argv[2]\n";
+		$number = mt_rand($argv[1], $argv[2]);
+		fwrite(STDOUT, "What number am I thinking?\n");
+		$guess = trim(fgets(STDIN));
+	do {
+		if ($guess > $number) {
+			fwrite(STDOUT, "Lower!\n");
+			$guess = trim(fgets(STDIN));
+			$count++;
+		} elseif ($guess < $number) {
+			fwrite(STDOUT, "Higher!\n");
+			$guess = trim(fgets(STDIN));
+			$count++;
+		} elseif ($guess == $number) {
+			fwrite(STDOUT, "You win!\n");
+			fwrite(STDOUT, "It took you $count tries.\n");
+			if (true) break;
+		}
+	} while ($guess !== $number);
+} else {
+	echo "Enter two numbers for your game silly\n", "First number must be lower than second\n";
 }
-do {
-	if ($guess > $number) {
-		fwrite(STDOUT, "Lower!\n");
-		$guess = trim(fgets(STDIN));
-		$count++;
-	} elseif ($guess < $number) {
-		fwrite(STDOUT, "Higher!\n");
-		$guess = trim(fgets(STDIN));
-		$count++;
-	} elseif ($guess == $number) {
-		fwrite(STDOUT, "You win!\n");
-		if (true) break;
-	}
-} while ($guess !== $number);
-fwrite(STDOUT, "It took you $count tries.\n");
