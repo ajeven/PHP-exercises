c11ddcd9688e0ff42091e00dea0106075d87954c
Alan <macbookpro@MacBooks-MacBook-Pro.local>
2016-07-06 09:39:32 -0500

    "Add break_continue with range function using foreach instead of for"

--- break_continue.php
+++ break_continue.php
@@ -15,3 +15,10 @@ for ($number = 1; $number <= 100; $number++) {
 	}	
 }
 echo "I wont go past 10! NO WAY NO HOW!\n";
+// use range for the same arguments as the for loop with a foreach!
+// foreach (range(1, 100) as $number) {
+// 	echo $number . "\n";
+// 	if ($number == 10) {
+// 		break;
+// 	}
+// }
