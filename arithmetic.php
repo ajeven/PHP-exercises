ffd3f458f8c31e340892fc1acbb94f844a4d3e31
Alan <macbookpro@MacBooks-MacBook-Pro.local>
2016-07-06 15:20:30 -0500

    "Added arguments to arithmetic and adding grades exercise"

--- arithmetic.php
+++ arithmetic.php
@@ -1,21 +1,46 @@
 <?php
 function add($a, $b) {
-    return ($a + $b) . "\n";
+	if (is_numeric($a) && is_numeric($b)) {
+		return ($a + $b) . "\n";
+	} else {
+		echo "Both values need to me numeric in value.\n";
+	}
 }
 function subtract($a, $b) {
-    return ($a - $b) . "\n";
+	if (is_numeric($a) && is_numeric($b)) {
+		return ($a - $b) . "\n";
+	} else {
+		echo "Both values need to me numeric in value.\n";
+	}
 }
 function multiply($a, $b) {
-    return ($a * $b) . "\n";
+	if (is_numeric($a) && is_numeric($b)) {
+		return ($a * $b) . "\n";
+	} else {
+		echo "Both values need to me numeric in value.\n";
+	}
 }
 function divide($a, $b) {
-    return ($a / $b) . "\n";
+	if (is_numeric($a) && is_numeric($b) && $b != 0) {
+		return ($a / $b) . "\n";
+	} elseif ($b == 0) {
+		echo "Can not divide by 0\n";
+	} else {
+		echo "Both values need to me numeric in value.\n";
+	}
 }
 function modulus($a, $b) {
-    return ($a % $b) . "\n";
+	if (is_numeric($a) && is_numeric($b) && $b != 0) {
+		return ($a % $b) . "\n";
+	} elseif ($b == 0) {
+		echo "Can not divide by 0\n";
+	} else {
+		echo "Both values need to me numeric in value.\n";
+	}
 }
+
 $a = 100;
-$b = 20;
+$b = 25;
 echo add($a, $b);
 echo subtract($a, $b);
 echo multiply($a, $b);
---
+++ grade.php
@@ -0,0 +1,17 @@
+<?php
+$subjects = [];
+$grades = [];
+fwrite(STDOUT, "Give me subjects and their grades and I will return an average\n");
+do {
+	fwrite(STDOUT, "What subject? \n");
+	array_push($subjects, fgets(STDIN));
+	fwrite(STDOUT, "What was your grade? \n");
+	array_push($grades, fgets(STDIN));
+	fwrite(STDOUT, "Add more? Y or N.\n");
+	$continue = trim(fgets(STDIN));
+} while ($continue == "y");
+echo "This is your grades added together.\n";
+echo $solution = array_sum($grades) . "\n";
+echo "This is your average.\n";
+$average = $solution / count($grades) . "\n";
+echo round($average) . "\n";
