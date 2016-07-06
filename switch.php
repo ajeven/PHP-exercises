1069a87db496c123e1817905ad338aedd814550e
Alan <macbookpro@MacBooks-MacBook-Pro.local>
2016-07-06 09:21:04 -0500

    "Add completed switch exercise"

--- switch.php
+++ switch.php
@@ -9,24 +9,39 @@ $dayOfWeek = date('N');
 
 switch($dayOfWeek) {
 	case 1:
-	 	echo "Monday\n";
-	 	break;
+		echo "Today is Monday\n";
+		break;
 	case 2:
-		echo "Tuesday\n";
+		echo "Today is Tuesday\n";
 		break;
 	case 3:
-		echo "Wednesday\n";
+		echo "Today is Wednesday\n";
 		break;
 	case 4:
-		echo "Thrusday\n";
+		echo "Today is Thrusday\n";
 		break;
 	case 5:
-		echo "Friday!\n";
+		echo "Today is Friday!\n";
 		break;
 	case 6:
-		echo "Saturday\n";
+		echo "Today is Saturday\n";
 		break;
 	case 7:
-		echo "Sunday\n";
+		echo "Today is Sunday\n";
 		break;
+}
+if ($dayOfWeek == '1') {
+	echo "Today is Monday\n";
+} elseif ($dayOfWeek == '2') {
+	echo "Today is Tuesday\n";
+} elseif ($dayOfWeek == '3') {
+	echo "Today is Wednesday\n";
+} elseif ($dayOfWeek == '4') {
+	echo "Today is Thrusday\n";
+} elseif ($dayOfWeek == '5') {
+	echo "Today is Friday\n";
+} elseif ($dayOfWeek == '6') {
+	echo "Today is Saturday\n";
+} elseif ($dayOfWeek == '7') {
+	echo "Today is Sunday\n";
 }
