8ce9cf9cd79dc99e2cd4563623d7c68ab1fee6e3
Alan <macbookpro@MacBooks-MacBook-Pro.local>
2016-07-07 10:57:01 -0500

    Modified grade.php and added error function to arithmetic

--- arithmetic.php
+++ arithmetic.php
@@ -2,47 +2,42 @@
 function add($a, $b) {
 	if (is_numeric($a) && is_numeric($b)) {
 		return ($a + $b) . "\n";
-	} else {
-		echo "Both values need to me numeric in value.\n";
 	}
 }
 function subtract($a, $b) {
 	if (is_numeric($a) && is_numeric($b)) {
 		return ($a - $b) . "\n";
-	} else {
-		echo "Both values need to me numeric in value.\n";
 	}
 }
 function multiply($a, $b) {
 	if (is_numeric($a) && is_numeric($b)) {
 		return ($a * $b) . "\n";
-	} else {
-		echo "Both values need to me numeric in value.\n";
 	}
 }
 function divide($a, $b) {
 	if (is_numeric($a) && is_numeric($b) && $b != 0) {
 		return ($a / $b) . "\n";
-	} elseif ($b == 0) {
-		echo "Can not divide by 0\n";
-	} else {
-		echo "Both values need to me numeric in value.\n";
 	}
 }
 function modulus($a, $b) {
 	if (is_numeric($a) && is_numeric($b) && $b != 0) {
 		return ($a % $b) . "\n";
+	}
+}
+function error($a, $b) {
+	if (is_numeric($a) && is_numeric($b) && $b != 0) {
+		echo add($a, $b);
+		echo subtract($a, $b);
+		echo multiply($a, $b);
+		echo divide($a, $b);
+		echo modulus($a, $b);
 	} elseif ($b == 0) {
-		echo "Can not divide by 0\n";
+		echo "Can not divide by 0!\n";
 	} else {
 		echo "Both values need to me numeric in value.\n";
 	}
 }
 
-$a = 100;
-$b = 25;
-echo add($a, $b);
-echo subtract($a, $b);
-echo multiply($a, $b);
-echo divide($a, $b);
-echo modulus($a, $b);
+$a = 40;
+$b = 20;
+echo error($a, $b);
--- grade.php
+++ grade.php
@@ -1,6 +1,7 @@
 <?php
 $subjects = [];
 $grades = [];
+$awesomeGrade = 80;
 fwrite(STDOUT, "Give me subjects and their grades and I will return an average\n");
 do {
 	fwrite(STDOUT, "What subject? \n");
@@ -9,9 +10,14 @@ do {
 	array_push($grades, fgets(STDIN));
 	fwrite(STDOUT, "Add more? Y or N.\n");
 	$continue = trim(fgets(STDIN));
-} while ($continue == "y");
+} while (strtolower($continue) == "y");
 echo "This is your grades added together.\n";
 echo $solution = array_sum($grades) . "\n";
 echo "This is your average.\n";
 $average = $solution / count($grades) . "\n";
 echo round($average) . "\n";
+if ($average >= $awesomeGrade) {
+	echo "You are doing great!\n";
+} else {
+	echo "You should practice more.\n";
+}
