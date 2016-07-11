dd00898571bd64bb345ccbfd587c1197f4583ad6
Alan <macbookpro@MacBooks-MacBook-Pro.local>
2016-07-11 16:50:51 -0500

    "Added logger exercise"

---
+++ logger.php
@@ -0,0 +1,23 @@
+<?php
+
+function logMessage($logLevel, $message)
+{
+	$date = date('Y-m-d');
+	$filename = "log-YYYY-MM-DD.log";
+	$handler = fopen($filename, 'a');
+	$contents = trim(filesize($filename));
+	if ($contents == 0) {
+		fwrite($handler, $date . $message) . "\n";
+	}
+	fwrite($handler, $date . $message) . "\n";
+	fclose($handler);
+}
+
+function logInfo($message = "info") {
+	logMessage("info", $message) . "\n";
+}
+function logError($message = "error") {
+	logMessage("error", $message) . "\n";
+}
+logInfo();
+logError();
