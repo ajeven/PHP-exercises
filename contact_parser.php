e0bfbf6126d2e57fb0473632327599d18b21a574
Alan <macbookpro@MacBooks-MacBook-Pro.local>
2016-07-11 15:11:22 -0500

    "finished read files exercise"

--- contact_parser.php
+++ contact_parser.php
@@ -1,11 +1,23 @@
 <?php
 
 function parseContacts($filename) {
-    $contacts = array();
+	$contacts = array();
+	// todo - read file and parse contacts
+	$filename = 'contacts.txt';
+	$handle = fopen($filename, 'r');
+	$contents = trim(fread($handle, filesize($filename)));
+	fclose($handle);
+	$contentsArray = (explode("\n", $contents));
 
-    // todo - read file and parse contacts
-
-    return $contacts;
+	foreach ($contentsArray as $content) {
+		$contact = [];
+		$content = explode("|", $content);
+		$dash = substr($content['1'], 0, 3) . "-" . substr($content['1'], 3, 3) . "-" . substr($content['1'], 6);
+		$contact['name'] = $content['0'];
+		$contact['number'] = $dash;
+		$contacts[] = $contact;
+	}
+	return $contacts;
 }
 
 var_dump(parseContacts('contacts.txt'));
