e4af179df580b00c1ddef299bc38f604bc082534
Alan <macbookpro@MacBooks-MacBook-Pro.local>
2016-07-12 09:33:50 -0500

    "Add sort exercise without using pre-defined sort functions for php"

---
+++ array_sort.php
@@ -0,0 +1,15 @@
+<?php
+$letters = array('e', 'a', 'g', 'x', 'i', 'd', 'f', 'b', 'h');
+
+for ($i = 0; $i < count($letters); $i++) {
+	for ($x = 0; $x < count($letters)-1; $x++) {
+
+		if ($letters[$x] > $letters[$x+1]) {
+			$sorted = $letters[$x+1];
+			$letters[$x+1] = $letters[$x];
+			$letters[$x] = $sorted;
+		}
+	}
+}
+
+print_r($letters);
